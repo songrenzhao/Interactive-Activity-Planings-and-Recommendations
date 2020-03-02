07d3fc9f0ce73de6cbab5fba89899c06965ad709
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2020-03-01 22:41:38 -0500

    added survey table, survey blog, modified survey

---
+++ src/pages/SurveyBlog/SurveyBlog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { SurveyList } from '../../components/SurveyBlog';
+
+export const SurveyBlog = () => (
+  <Grid container justify="center">
+    <SurveyList />
+  </Grid>
+);
