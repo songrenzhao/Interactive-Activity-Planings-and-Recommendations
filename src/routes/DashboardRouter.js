688d333c83c6823da7a5e8316bdad64424300f71
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2020-03-15 22:13:25 -0400

    add modified page and dashboardroute

--- src/routes/DashboardRouter.js
+++ src/routes/DashboardRouter.js
@@ -3,6 +3,7 @@ import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { SurveyForm } from '../components/Survey';
 import { SurveyList } from '../components/SurveyBlog';
 import { SurveyTable } from '../components/SurveyTable';
+import { CreateParticipant } from '../components/CreateParticipant';
 
 const DashboardRouter = () => {
   const { path } = useRouteMatch();
@@ -16,6 +17,9 @@ const DashboardRouter = () => {
       </Route>
       <Route path={`${path}/table`}>
         <SurveyTable />
+      </Route>
+      <Route path={`${path}/participant`}>
+        <CreateParticipant />
       </Route>
     </Switch>
   );
