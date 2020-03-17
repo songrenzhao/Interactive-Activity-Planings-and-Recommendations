3549ae00cd18fc4b8b4abcde1da33dcfd02f279d
Songren Zhao <songrenzhao@gmail.com>
2020-03-17 18:25:58 -0400

    finalized with UI, working on backend

--- src/components/Forms/CreateSurveyForm/CreateSurvey.js
+++ src/components/Forms/CreateSurveyForm/CreateSurvey.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Button from '@material-ui/core/Button';
@@ -9,11 +10,13 @@ import Tab from '@material-ui/core/Tab';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import { Grid } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
 import useStyles from './style';
 
 export const CreateSurvey = () => {
   const classes = useStyles();
   const [activeIndex, setActiveIndex] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(false);
   const [formData, setFormData] = React.useState([
     {
       title: '',
@@ -82,7 +85,7 @@ export const CreateSurvey = () => {
   };
 
   const handleMoreTab = () => {
-    const updatedFormData = formData;
+    const updatedFormData = [...formData];
     updatedFormData.push({
       title: '',
       description: '',
@@ -94,6 +97,31 @@ export const CreateSurvey = () => {
       }],
     });
     setFormData(updatedFormData);
+  };
+
+  const uploadImage = (selectionIndex) => async (e) => {
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'IAPRLIVE');
+    setIsLoading(true);
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/iapr/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      },
+    );
+    const { secure_url } = await res.json();
+    const updatedFormData = [...formData];
+    updatedFormData[activeIndex].selections[selectionIndex].url = secure_url;
+    setFormData(updatedFormData);
+    setIsLoading(false);
+  };
+
+  const submitSurvey = (e) => {
+    e.preventDefault();
+    console.log(formData);
   };
 
   const TabPanel = ({ data, index }) => {
@@ -116,7 +144,6 @@ export const CreateSurvey = () => {
                 name={`title-${index}`}
                 defaultValue={title}
                 onChange={handleFormDataChange('title')}
-                autoFocus
               />
               <TextField
                 variant="outlined"
@@ -134,6 +161,7 @@ export const CreateSurvey = () => {
                 margin="normal"
                 required
                 fullWidth
+                type="number"
                 name={`limit-${index}`}
                 label="Limit"
                 id={`limit-${index}`}
@@ -151,30 +179,43 @@ export const CreateSurvey = () => {
                 defaultValue={question}
                 id={`question-${index}`}
               />
-              <Paper>
-                <Grid container className={classes.FormGrid}>
-                  {selections.map((selection, selectionIndex) => (
-                    <Grid item component={Paper} xs={12} sm={12} className={classes.selectionFormGrid}>
-                      <TextField
-                        required
-                        id={`selection-${index}-${selectionIndex}`}
-                        name={`selection-${index}-${selectionIndex}`}
-                        label="Selection"
-                        onChange={handleSelectionChange(selectionIndex)}
-                        defaultValue={selection.choice}
+              <Grid container className={classes.FormGrid}>
+                {selections.map((selection, selectionIndex) => (
+                  <Grid item component={Paper} spacing={2} xs={12} sm={12} className={classes.selectionFormGrid}>
+                    <TextField
+                      required
+                      id={`selection-${index}-${selectionIndex}`}
+                      name={`selection-${index}-${selectionIndex}`}
+                      label="Selection"
+                      onChange={handleSelectionChange(selectionIndex)}
+                      defaultValue={selection.choice}
+                    />
+                    <Button variant="outlined" color="primary" component="label">
+                      Upload File
+                      <input
+                        type="file"
+                        name="file"
+                        style={{ display: 'none' }}
+                        onChange={uploadImage(selectionIndex)}
                       />
-                      <Button variant="outlined" color="primary" href="#outlined-buttons">
-                        Select Image
-                      </Button>
-                    </Grid>
-                  ))}
-                  <Grid item xs={6} sm={6} className={classes.selectionFormGrid}>
-                    <Button variant="outlined" color="primary" href="#outlined-buttons" onClick={handleMoreSelection}>
-                      Add new selection
                     </Button>
+                    {isLoading ? (
+                      <h3>Loading</h3>
+                    ) : (
+                      <CardMedia
+                        className={classes.media}
+                        image={selection.url}
+                        title="Contemplative Reptile"
+                      />
+                    )}
                   </Grid>
+                ))}
+                <Grid item xs={6} sm={6} className={classes.selectionFormGrid}>
+                  <Button variant="outlined" color="primary" href="#outlined-buttons" onClick={handleMoreSelection}>
+                    Add new selection
+                  </Button>
                 </Grid>
-              </Paper>
+              </Grid>
             </>
           )
         }
@@ -203,17 +244,24 @@ export const CreateSurvey = () => {
             </Fab>
           </Tabs>
         </Paper>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitSurvey}>
           {formData.map((data, index) => (
             <TabPanel data={data} index={index} />
           ))}
           <Button
-            fullWidth
-            variant="contained"
+            style={{ marginLeft: '25%', width: '50%' }}
             color="primary"
             onClick={handleNext}
           >
             Next
+          </Button>
+          <Button
+            type="submit"
+            style={{ marginTop: '20px', marginLeft: '30%', width: '40%' }}
+            variant="contained"
+            color="primary"
+          >
+            Submit
           </Button>
         </form>
       </Grid>
