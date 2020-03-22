eda469310cccd4a72cffa2b4b34e7b88bbe752ff
hjmoon12 <47199715+hjmoon12@users.noreply.github.com>
2020-03-22 15:14:36 -0400

    Add files via upload

---
+++ src/pages/homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import homepage from "./homepage";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
